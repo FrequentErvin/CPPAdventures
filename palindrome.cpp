36f223b890112c6e66c0ee120496b9eb114a80e2
FrequentErvin <75392265+FrequentErvin@users.noreply.github.com>
2021-05-12 16:36:34 +0200

    first commit

---
+++ palindrome.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    /*string s;
+    cin >> s;
+    //greedy approach:*/
+    int n;
+    cout << "unesite kerove_ " << endl;
+    cin >> n;
+
+}
