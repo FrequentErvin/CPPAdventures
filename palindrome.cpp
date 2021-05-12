5ed39d4e2cdf17c9cd0ed15cebf847603d26f56d
FrequentErvin <75392265+FrequentErvin@users.noreply.github.com>
2021-05-12 17:03:09 +0200

    experimenting

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,11 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    /*string s;
+    string s;
     cin >> s;
-    //greedy approach:*/
-    int n;
-    cout << "unesite kerove_ " << endl;
-    cin >> n;
-
+    //greedy approach:
+    int n = s.length();
+    map<int,int> hash;
+    for(int i = 0; i < s.length(); i++){
+        int leftodd=i, rightodd=i;
+        while(leftodd >=0 and rightodd <= n-1){
+            if(s[leftodd] == s[rightodd]){
+                hash[i]++;
+            }else{
+                break;
+            }
+            leftodd++;
+            rightodd++;
+        }
+        int lefteven = i, righteven = i;
+        while(lefteven >= 0 and rightodd <= n-1){
+            if(s[lefteven] == s[righteven]){
+                hash[i]++;
+            }else{
+                break;
+            }
+            lefteven++;
+            righteven++;
+        }
+    }
 }
