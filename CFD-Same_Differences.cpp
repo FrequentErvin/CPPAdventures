567b892520a600d3a112d3cac33af93520ab9bef
FrequentErvin <75392265+FrequentErvin@users.noreply.github.com>
2021-05-14 00:10:27 +0200

    sleeping now, this is where I left off

---
+++ CFD-Same_Differences.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define ll long long
+#define mp make_pair
+using namespace std;
+
+int main(){
+ios_base::sync_with_stdio(false); cin.tie(NULL);
+ int t;
+ cin>>t;
+  while(t--){
+      int n;
+      cin >> n;
+      int a[n];
+      map<int,int> hash;
+      for(int i = 0; i < n; i++){
+          cin >> a[i];
+      }
+
+  }
+}
--- palindrome.cpp
+++ palindrome.cpp
@@ -1,76 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+int LCS(string s, string q, int m, int n){ 
+    int ans[m+1][1+n]; 
+    for(int i = 0; i <= m; i++) { 
+        for(int j = 0; j <= n; j++){ 
+        if(i == 0 or j == 0){
+            ans[i][j] = 0; 
+        }else if(s[i-1] == q[j-1]){
+          ans[i][j] = ans[i-1][j-1] + 1; 
+        }else{
+            ans[i][j] = max(ans[i-1][j], ans[i][j-1]); 
+        }
+        } 
+    } 
+    return ans[m][n]; 
+} 
 int main(){
+    int n; 
+    cin >> n;
     string s;
     cin >> s;
-    //greedy approach:
-    int n = s.length();
-    map<int,int> hash;
-    map<int,int> hash2;
-    int ans[n] = {0};
-    for(int i = 0; i < s.length(); i++){
-        int leftodd=i;
-        int rightodd=i;
-        while(leftodd >=0 and rightodd <= n-1){
-            if(s[leftodd] == s[rightodd]){
-                hash[i]++;
-            }else{
-                break;
-            }
-            leftodd--;
-            rightodd++;
-        }
-        int lefteven = i;
-        int righteven = i+1;
-        while(lefteven >= 0 and righteven <= n-1){
-            if(s[lefteven] == s[righteven]){
-                hash2[i]++;
-            }else{
-                break;
-            }
-            lefteven--;
-            righteven++;
-        }
-        ans[i] = max(hash[i], hash2[i]);
+    string q = "";
+    for(int i = n-1; i >= 0; i--){
+        q += s[i];
     }
-    int max = 0;
-    for(int i = 0; i < n; i++){
-        if(max <= ans[i]){
-            max = ans[i];
-        }
-    }
-    vector<int> index;
-    for(int i = 0; i < n; i++){
-        if(ans[i] == max){
-            index.push_back(i);
-        }
-    }
-    //int endsol[(int)index.size()+1];
-    //new line, for testing purposes
-    set<int> endsol;
-    for(int i = 0; i < (int)index.size(); i++){
-        int leftodd1 = i, rightodd1 = i;
-        int solodd = 0;
-        int soleven = 0;
-        while(leftodd1 >= 0 and rightodd1 <= n-1){
-            if(s[leftodd1] != s[rightodd1]){
-                solodd += 2;
-            }
-            leftodd1--;
-            rightodd1++;
-        }
-        int lefteven2 = i; 
-        int rightodd2 = i+1;
-        while(lefteven2 >= 0 and righteven2 <= n-1){
-            if(s[lefteven2] != s[righteven2]){
-                soleven += 2;
-            }
-            lefteven2--;
-            righteven2++;
-        }
-    endsol.insert(min(solodd, soleven));
-    }
-    set<int>::iterator itr;
-    itr = endsol.begin();
-    cout << *itr << endl;
+    /*for(int i = 0; i < (int)q.size(); i++){
+        cout << q[i] << endl;
+    }*/
+    cout << n - LCS(s,q,n,n) << endl;
 }
