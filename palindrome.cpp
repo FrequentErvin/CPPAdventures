67db326c1825e688aa412c616c600514204e41b2
FrequentErvin <75392265+FrequentErvin@users.noreply.github.com>
2021-05-13 19:28:20 +0200

    getting used to vscode again..

--- palindrome.cpp
+++ palindrome.cpp
@@ -6,26 +6,70 @@ int main(){
     //greedy approach:
     int n = s.length();
     map<int,int> hash;
+    map<int,int> hash2;
+    int ans[n] = {0};
     for(int i = 0; i < s.length(); i++){
-        int leftodd=i, rightodd=i;
+        int leftodd=i;
+        int rightodd=i;
         while(leftodd >=0 and rightodd <= n-1){
             if(s[leftodd] == s[rightodd]){
                 hash[i]++;
             }else{
                 break;
             }
-            leftodd++;
+            leftodd--;
             rightodd++;
         }
-        int lefteven = i, righteven = i;
-        while(lefteven >= 0 and rightodd <= n-1){
+        int lefteven = i;
+        int righteven = i+1;
+        while(lefteven >= 0 and righteven <= n-1){
             if(s[lefteven] == s[righteven]){
-                hash[i]++;
+                hash2[i]++;
             }else{
                 break;
             }
-            lefteven++;
+            lefteven--;
             righteven++;
         }
+        ans[i] = max(hash[i], hash2[i]);
     }
+    int max = 0;
+    for(int i = 0; i < n; i++){
+        if(max <= ans[i]){
+            max = ans[i];
+        }
+    }
+    vector<int> index;
+    for(int i = 0; i < n; i++){
+        if(ans[i] == max){
+            index.push_back(i);
+        }
+    }
+    //int endsol[(int)index.size()+1];
+    set<int> endsol;
+    for(int i = 0; i < (int)index.size(); i++){
+        int leftodd1 = i, rightodd1 = i;
+        int solodd = 0;
+        int soleven = 0;
+        while(leftodd1 >= 0 and rightodd1 <= n-1){
+            if(s[leftodd1] != s[rightodd1]){
+                solodd += 2;
+            }
+            leftodd1--;
+            rightodd1++;
+        }
+        int lefteven2 = i; 
+        int rightodd2 = i+1;
+        while(lefteven2 >= 0 and righteven2 <= n-1){
+            if(s[lefteven2] != s[righteven2]){
+                soleven += 2;
+            }
+            lefteven2--;
+            righteven2++;
+        }
+    endsol.insert(min(solodd, soleven));
+    }
+    set<int>::iterator itr;
+    itr = endsol.begin();
+    cout << *itr << endl;
 }
